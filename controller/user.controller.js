652ba565d1b2112293df350bc0f0599b340d5112
Yritte <tungclaozz@gmail.com>
2020-02-17 16:49:26 +0700

    17.02.2020

---
+++ controller/user.controller.js
@@ -0,0 +1,34 @@
+const db = require('../db')
+const shortid = require('shortid')
+
+module.exports.index = (req, res) => res.render('users/index', { 
+    users : db.get('users').value()
+})
+
+module.exports.search = (req, res) => {
+    let q = req.query.q;
+    let matchedUser = db.get('users').value().filter((user) =>{
+        return user.name.toLowerCase().indexOf(q.toLowerCase())!== -1;
+    });
+
+    res.render('users/index', { 
+        query : q,
+        users : matchedUser
+    })
+}
+
+module.exports.create = (req,res) => res.render('users/create')
+
+module.exports.get = (req,res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({ id: id}).value();
+    res.render('users/view', {
+        user: user
+    })
+}
+
+module.exports.postCreate = (req,res) => {
+    req.body.id = shortid.generate()
+    db.get('users').push(req.body).write()
+    res.redirect('/users')
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const userRoute = require('./routes/use.route')
+
+const port = 3000
+
+const app = express()
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded 
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+
+app.get('/', (req, res) => res.render('index'))
+
+app.use('/users', userRoute)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
